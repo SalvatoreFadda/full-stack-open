a61d0d85aaed9e6f08e6000e2f5a637df23d275b
SalvatoreFadda <37809998+SalvatoreFadda@users.noreply.github.com>
2023-04-07 14:24:28 +0200

    Add part2 course informations

---
+++ part2/course_information/README.md
@@ -0,0 +1,6 @@
+# full-stack-open-2022/2023
+
+Salvatore Fadda
+
+Full Stack Web Development part2 - Course information directory.
+Exercises 2.1 to 2.5
---
+++ part2/course_information/src/components/Course.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Header = ({ name }) => {
+  return <h1>{name}</h1>
+}
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  )
+}
+
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0)
+  return <strong>total of {totalExercises} exercises</strong>
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
